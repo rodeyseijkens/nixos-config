4abaf8197397e71f015e3b82408d2ec14f494ac2
Rodey Seijkens <me@rodey.nl>
2025-03-19 19:41:05 +0100

    feat: :wrench: update vscode configuration with new extensions and user settings

--- modules/home/vscode.nix
+++ modules/home/vscode.nix
@@ -14,77 +14,95 @@ in {
   config = mkIf cfg.enable {
     programs.vscode = {
       enable = true;
-      extensions =
-        (with pkgs.vscode-extensions; [
-          # nix language
-          bbenoist.nix
 
-          # nix formatting
-          kamadorueda.alejandra
-        ])
-        ++ (with marketplace; [
-          github.copilot
+      mutableExtensionsDir = true;
 
-          vivaxy.vscode-conventional-commits # commit message helper
-          eamodio.gitlens
+      profiles.default = {
+        extensions =
+          (with pkgs.vscode-extensions; [
+            # nix language
+            bbenoist.nix
 
-          # Color theme
-          jdinhlife.gruvbox
-          jonathanharty.gruvbox-material-icon-theme
-        ])
-        ++ (with marketplace-release; [
-          github.copilot-chat
-        ]);
-      userSettings = {
-        "update.mode" = "none";
+            # nix formatting
+            kamadorueda.alejandra
+          ])
+          ++ (with marketplace; [
+            # github.copilot
 
-        "editor.fontFamily" = "'Maple Mono', 'SymbolsNerdFont', 'monospace', monospace";
-        "editor.fontLigatures" = true;
-        "editor.fontSize" = 18;
+            vivaxy.vscode-conventional-commits # commit message helper
+            eamodio.gitlens
+            dbaeumer.vscode-eslint
+            esbenp.prettier-vscode
 
-        "editor.formatOnPaste" = true;
-        "editor.formatOnSave" = true;
-        "editor.formatOnType" = true;
+            # Color theme
+            jdinhlife.gruvbox
+            jonathanharty.gruvbox-material-icon-theme
+          ])
+          ++ (with marketplace-release; [
+            # github.copilot-chat
+          ]);
+        userSettings = {
+          "update.mode" = "none";
 
-        "editor.minimap.enabled" = true;
-        "editor.mouseWheelZoom" = true;
-        "editor.renderControlCharacters" = false;
-        "editor.scrollbar.horizontalScrollbarSize" = 5;
-        "editor.scrollbar.verticalScrollbarSize" = 5;
+          "editor.fontFamily" = "'Maple Mono', 'SymbolsNerdFont', 'monospace', monospace";
+          "editor.fontLigatures" = true;
+          "editor.fontSize" = 18;
 
-        "explorer.confirmDragAndDrop" = false;
-        "explorer.openEditors.visible" = 0;
+          "editor.formatOnPaste" = true;
+          "editor.formatOnSave" = true;
+          "editor.formatOnType" = true;
 
-        "extensions.autoUpdate" = false; # This stuff fixes vscode freaking out when theres an update
+          "editor.codeActionsOnSave" = {
+            "source.fixAll.eslint" = "explicit";
+          };
 
-        "files.autoSave" = "onWindowChange";
-        "terminal.integrated.fontFamily" = "'Maple Mono', 'SymbolsNerdFont'";
-        "vsicons.dontShowNewVersionMessage" = true;
+          "editor.minimap.enabled" = true;
+          "editor.mouseWheelZoom" = true;
+          "editor.renderControlCharacters" = false;
+          "editor.scrollbar.horizontalScrollbarSize" = 5;
+          "editor.scrollbar.verticalScrollbarSize" = 5;
 
-        "window.customTitleBarVisibility" = "auto";
-        "window.menuBarVisibility" = "toggle";
+          "explorer.confirmDragAndDrop" = false;
+          "explorer.openEditors.visible" = 0;
 
-        "workbench.activityBar.location" = "top";
-        "workbench.colorTheme" = "Gruvbox Dark Hard";
-        "workbench.editor.limit.enabled" = true;
-        "workbench.editor.limit.perEditorGroup" = true;
-        "workbench.editor.limit.value" = 10;
-        "workbench.iconTheme" = "gruvbox-material-icon-theme";
-        "workbench.layoutControl.enabled" = false;
-        "workbench.layoutControl.type" = "menu";
-        "workbench.startupEditor" = "none";
-        "workbench.statusBar.visible" = false;
+          "extensions.autoUpdate" = false; # This stuff fixes vscode freaking out when theres an update
 
-        # Extension settings
-        "material-icon-theme.folders.theme" = "classic";
+          "files.autoSave" = "onWindowChange";
+          "terminal.integrated.fontFamily" = "'Maple Mono', 'SymbolsNerdFont'";
+          "vsicons.dontShowNewVersionMessage" = true;
+
+          "window.customTitleBarVisibility" = "auto";
+          "window.menuBarVisibility" = "toggle";
+
+          "workbench.activityBar.location" = "top";
+          "workbench.colorTheme" = "Gruvbox Dark Hard";
+          "workbench.editor.limit.enabled" = true;
+          "workbench.editor.limit.perEditorGroup" = true;
+          "workbench.editor.limit.value" = 10;
+          "workbench.iconTheme" = "gruvbox-material-icon-theme";
+          "workbench.layoutControl.enabled" = false;
+          "workbench.layoutControl.type" = "menu";
+          "workbench.startupEditor" = "none";
+          "workbench.statusBar.visible" = true;
+
+          # Extension settings
+          "alejandra.program" = "alejandra";
+          "material-icon-theme.folders.theme" = "classic";
+          "[nix]" = {
+            "editor.defaultFormatter" = "kamadorueda.alejandra";
+            "editor.formatOnPaste" = true;
+            "editor.formatOnSave" = true;
+            "editor.formatOnType" = false;
+          };
+        };
+        # Keybindings
+        keybindings = [
+          {
+            key = "ctrl+s";
+            command = "workbench.action.files.saveFiles";
+          }
+        ];
       };
-      # Keybindings
-      keybindings = [
-        {
-          key = "ctrl+s";
-          command = "workbench.action.files.saveFiles";
-        }
-      ];
     };
   };
 }
