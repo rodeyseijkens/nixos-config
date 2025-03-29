4e6b6b994b68ee1188e116e9393328760dd48290
Rodey Seijkens <me@rodey.nl>
2025-03-30 00:42:12 +0100

    feat: :sparkles: add necessary DBus packages

--- modules/core/services.nix
+++ modules/core/services.nix
@@ -1,4 +1,4 @@
-{...}: {
+{pkgs, ...}: {
   services = {
     devmon.enable = true;
     gvfs.enable = true;
@@ -6,6 +6,12 @@
     gnome.gnome-keyring.enable = true;
     dbus.enable = true;
     fstrim.enable = true;
+
+    # needed for GNOME services outside of GNOME Desktop
+    dbus.packages = with pkgs; [
+      gcr
+      gnome-settings-daemon
+    ];
   };
   services.logind.extraConfig = ''
     # don’t shutdown when power button is short-pressed
