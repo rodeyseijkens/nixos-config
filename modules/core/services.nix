c04340055296dbd6926f81ac8680d0d3afa20fc9
Rodey Seijkens <me@rodey.nl>
2025-03-29 17:12:53 +0100

    feat: :sparkles: enable devmon and udisks2 services

--- modules/core/services.nix
+++ modules/core/services.nix
@@ -1,7 +1,8 @@
-{ ... }: 
-{
+{...}: {
   services = {
+    devmon.enable = true;
     gvfs.enable = true;
+    udisks2.enable = true;
     gnome.gnome-keyring.enable = true;
     dbus.enable = true;
     fstrim.enable = true;
