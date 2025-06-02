b4325627ba7c8fa0ddf7a0979d0e6a798c51b9a4
Rodey Seijkens <me@rodey.nl>
2025-06-02 19:13:22 +0200

    fix: :bug: add comments for clarity in packages.nix

--- modules/home/packages.nix
+++ modules/home/packages.nix
@@ -9,43 +9,43 @@
     entr # perform action when file change
     eza # ls replacement
     fd # find replacement
-    ffmpeg
+    ffmpeg # multimedia framework
     file # Show file information
     imv # image viewer
     killall
-    lazygit
+    lazygit # terminal git UI
     libnotify # Notification library
     man-pages # extra man pages
     mpv # video player
     ncdu # disk space
     nitch # systhem fetch util
     nix-prefetch-github
-    openssl
+    openssl # cryptographic library
     pamixer # pulseaudio command line mixer
     playerctl # controller for media players
-    unzip
-    wget
+    unzip # unzip utility
+    wget # file downloader
     wl-clipboard # clipboard utils for wayland (wl-copy, wl-paste)
-    xdg-utils
+    xdg-utils # utilities for desktop integration
     yazi # terminal file manager
     yt-dlp-light # audio/video downloader
-    onefetch
+    onefetch # git repository information tool
     gifsicle # gif utility - for record.sh
     zenity # simple dialog creator - for record.sh
     swappy # snapshot editing tool - for screenshot.sh
 
     ## CLI fun
     cbonsai # terminal screensaver
-    cmatrix
+    cmatrix # terminal screensaver
     pipes # terminal screensaver
-    sl
+    sl # steam locomotive
     tty-clock # cli clock
 
     ## GUI Apps
     bleachbit # cache cleaner
     pwvucontrol # pipewire volume controle (GUI)
-    vlc
-    mission-center
+    vlc # video player
+    mission-center # system monitor
 
     # Python
     python3
@@ -57,6 +57,6 @@
     ## Misc
     winetricks
     wineWowPackages.wayland
-    inputs.alejandra.defaultPackage.${system}
+    inputs.alejandra.defaultPackage.${system} # syntax highlighter for nix
   ];
 }
