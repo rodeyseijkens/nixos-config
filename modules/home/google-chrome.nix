b5af83da788e24b4b4b66bf6e45967a37533c138
Rodey Seijkens <me@rodey.nl>
2025-06-16 16:40:46 +0200

    fix: :ambulance: fix chrome crashing when pasting

--- modules/home/google-chrome.nix
+++ modules/home/google-chrome.nix
@@ -12,6 +12,10 @@ in {
     programs.chromium = {
       enable = true;
       package = pkgs.google-chrome;
+      commandLineArgs = [
+        "--enable-features=UseOzonePlatform"
+        "--ozone-platform=wayland"
+      ];
       # extensions = [
       #   {id = "cjpalhdlnbpafiamejdnhcphjbkeiagm";} # ublock origin
       #   {id = "hfjbmagddngcpeloejdejnfgbamkjaeg";} # vimium-c
