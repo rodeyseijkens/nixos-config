db2a5a766a3012ba9371701a53090a0666d145f8
Rodey Seijkens <me@rodey.nl>
2025-07-12 13:00:38 +0200

    refactor: :wrench: reorganize home-configuration entries

--- hosts/desktop-work/home-configuration.nix
+++ hosts/desktop-work/home-configuration.nix
@@ -1,11 +1,18 @@
 {...}: {
   modules = {
+    # Browser
     zen-browser.enable = true;
+    google-chrome.enable = true;
+
+    # Code editors
     vscode.enable = true;
+
+    # File managers
     nautilus.enable = true;
+
+    # Misc
     spicetify.enable = true;
     legcord.enable = true;
-    google-chrome.enable = true;
     tigervnc.enable = true;
   };
 }
