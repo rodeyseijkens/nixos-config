3acd1ae48aaf2e610037defadc48c1d19be1e71f
Rodey Seijkens <me@rodey.nl>
2025-07-12 13:46:27 +0200

    refactor(flatpak): :wrench: restructure flatpak configuration and add flatpak package list

--- modules/core/flatpak.nix
+++ modules/core/flatpak.nix
@@ -1,17 +1,34 @@
-{ inputs, pkgs, ... }:
 {
-  imports = [ inputs.nix-flatpak.nixosModules.nix-flatpak ];
+  inputs,
+  lib,
+  pkgs,
+  config,
+  ...
+}:
+with lib; let
+  flatpakPackages = config.flatpaks;
+in {
+  imports = [inputs.nix-flatpak.nixosModules.nix-flatpak];
 
-  services.flatpak = {
-    enable = true;
-    packages = [
-      # "io.github.zen_browser.zen"
-      "com.github.tchx84.Flatseal"
-    ];
-    overrides = {
-      global = {
-        # Force Wayland by default
-        Context.sockets = ["wayland" "!x11" "!fallback-x11"];
+  options.flatpaks = mkOption {
+    type = types.listOf types.str;
+    default = [];
+    description = "List of Flatpak packages to install";
+  };
+
+  config = mkIf (flatpakPackages != []) {
+    services.flatpak = {
+      enable = true;
+      packages =
+        [
+          "com.github.tchx84.Flatseal"
+        ]
+        ++ flatpakPackages;
+      overrides = {
+        global = {
+          # Force Wayland by default
+          Context.sockets = ["wayland" "!x11" "!fallback-x11"];
+        };
       };
     };
   };
