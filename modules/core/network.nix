55c9be33cdcfba755bbf43c061834fd6820ac60c
Rodey Seijkens <me@rodey.nl>
2025-07-29 12:00:23 +0200

    fix(network): :bug: update nameservers and disabled ipv6

--- modules/core/network.nix
+++ modules/core/network.nix
@@ -1,14 +1,18 @@
-{ pkgs, host, ... }: 
 {
+  pkgs,
+  host,
+  ...
+}: {
   networking = {
     hostName = "${host}";
     networkmanager.enable = true;
-    nameservers = [ "8.8.8.8" "8.8.4.4" "1.1.1.1" ];
+    nameservers = ["1.1.1.1" "1.0.0.1"];
     firewall = {
       enable = true;
-      allowedTCPPorts = [ 22 80 443 ];
-      allowedUDPPorts = [  ];
+      allowedTCPPorts = [22 80 443];
+      allowedUDPPorts = [];
     };
+    enableIPv6 = false;
   };
 
   environment.systemPackages = with pkgs; [
