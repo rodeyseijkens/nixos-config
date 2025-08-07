ee65e72793008a1dcdb29d2c5a83269a12bdb275
Rodey Seijkens <me@rodey.nl>
2025-08-08 01:22:20 +0200

    feat(vscode): :wrench: add default formatter for CSS files

--- modules/home/vscode.nix
+++ modules/home/vscode.nix
@@ -107,6 +107,9 @@ in {
           "[typescriptreact]" = {
             "editor.defaultFormatter" = "esbenp.prettier-vscode";
           };
+          "[css]" = {
+            "editor.defaultFormatter" = "esbenp.prettier-vscode";
+          };
         };
         # Keybindings
         keybindings = [
