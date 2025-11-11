bd09c50aa8482b1bd649957914e5690908f28492
Rodey Seijkens <me@rodey.nl>
2025-11-11 10:07:49 +0100

    chore: :alien: rename vaapiVdpau to libva-vdpau-driver

--- modules/core/hardware.nix
+++ modules/core/hardware.nix
@@ -11,7 +11,7 @@ in {
       enable = true;
       enable32Bit = true;
       extraPackages = with pkgs; [
-        vaapiVdpau
+        libva-vdpau-driver
         libvdpau
         libvdpau-va-gl
         nvidia-vaapi-driver
