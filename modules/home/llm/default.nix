c51dbbc9aa26ce7b9992808deb99617da59c391d
Rodey Seijkens <me@rodey.nl>
2026-03-07 11:28:50 +0100

    feat(llm): :sparkles: add default model configuration

--- modules/home/llm/default.nix
+++ modules/home/llm/default.nix
@@ -2,12 +2,16 @@
   inputs,
   pkgs,
   ...
-}: {
+}: let
+  defaultModel = "openrouter/openrouter/free";
+in {
   home.packages = with pkgs; [
     ## LLM Tools
-    (python313Packages.llm.withPlugins {
+    (llm.withPlugins {
       # LLM plugin for models hosted by OpenRouter
       llm-openrouter = true;
     })
   ];
+
+  xdg.configFile."io.datasette.llm/default_model.txt".text = "${defaultModel}\n";
 }
