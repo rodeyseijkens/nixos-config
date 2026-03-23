3bc8e3e6d6494615fc59e473e55bf1207e3b53fd
Rodey Seijkens <me@rodey.nl>
2026-03-23 13:37:53 +0100

    feat(fonts): change to stylix only font management

--- modules/home/gtk.nix
+++ modules/home/gtk.nix
@@ -4,18 +4,6 @@
   lib,
   ...
 }: {
-  fonts.fontconfig.enable = true;
-  home.packages = with pkgs; [
-    nerd-fonts.jetbrains-mono
-    nerd-fonts.fira-code
-    nerd-fonts.caskaydia-cove
-    nerd-fonts.symbols-only
-    twemoji-color-font
-    noto-fonts-color-emoji
-    fantasque-sans-mono
-    maple-mono.truetype-autohint
-  ];
-
   gtk = {
     enable = true;
     # font = {
