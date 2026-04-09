b026133c790b07b251b42177c441a61a1c82e1f8
Rodey Seijkens <me@rodey.nl>
2026-04-09 10:02:02 +0200

    chore: :wrench: update monitor references

--- hosts/desktop-office/home-configuration.nix
+++ hosts/desktop-office/home-configuration.nix
@@ -3,15 +3,15 @@
     enable = true;
     config = ''
       monitor = DP-3, 1920x1080@60, -1080x-420, 1, transform, 3
-      monitor = DP-2, 1920x1080@60, 0x0, 1
+      monitor = DP-1, 1920x1080@60, 0x0, 1
       monitor = DP-4, 1920x1080@60, 1920x0, 1
     '';
     workspaces = [
-      "1, monitor:DP-2, default:true"
-      "2, monitor:DP-2"
-      "3, monitor:DP-2"
-      "4, monitor:DP-2"
-      "5, monitor:DP-2"
+      "1, monitor:DP-1, default:true"
+      "2, monitor:DP-1"
+      "3, monitor:DP-1"
+      "4, monitor:DP-1"
+      "5, monitor:DP-1"
       "8, monitor:DP-3"
       "9, monitor:DP-3, default:true"
       "10, monitor:DP-3"
