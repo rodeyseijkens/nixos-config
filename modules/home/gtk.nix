9de94ce7998445b52063ed96d8cd2196e7deeb6b
Rodey Seijkens <me@rodey.nl>
2026-06-23 11:23:55 +0200

    chore: :package: update flake.lock

--- modules/home/gtk.nix
+++ modules/home/gtk.nix
@@ -21,7 +21,6 @@
     gtk3.extraConfig = {
       gtk-application-prefer-dark-theme = 1;
     };
-    gtk4.theme = config.gtk.theme;
     gtk4.extraConfig = {
       gtk-application-prefer-dark-theme = 1;
     };
