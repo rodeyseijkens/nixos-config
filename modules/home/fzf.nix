b8173161270d31c38946ea522be02dcfe59aa8c3
Rodey Seijkens <me@rodey.nl>
2026-07-07 17:20:44 +0200

    chore(fzf): changed to new config style

--- modules/home/fzf.nix
+++ modules/home/fzf.nix
@@ -6,9 +6,9 @@
     enableZshIntegration = true;
     
     defaultCommand = "fd --hidden --strip-cwd-prefix --exclude .git";
-    fileWidgetOptions = [ "--preview 'if [ -d {} ]; then eza --tree --color=always {} | head -200; else bat -n --color=always --line-range :500 {}; fi'" ];
-    changeDirWidgetCommand = "fd --type=d --hidden --strip-cwd-prefix --exclude .git";
-    changeDirWidgetOptions = [ "--preview 'eza --tree --color=always {} | head -200'" ];
+    fileWidget.options = [ "--preview 'if [ -d {} ]; then eza --tree --color=always {} | head -200; else bat -n --color=always --line-range :500 {}; fi'" ];
+    changeDirWidget.command = "fd --type=d --hidden --strip-cwd-prefix --exclude .git";
+    changeDirWidget.options = [ "--preview 'eza --tree --color=always {} | head -200'" ];
 
     ## Theme
     defaultOptions = [
