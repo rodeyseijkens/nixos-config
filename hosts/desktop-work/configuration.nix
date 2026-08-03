8f24a96d13c260e48e95d6face41cdc8a7dcd4e5
Rodey Seijkens <me@rodey.nl>
2026-08-03 16:35:58 +0200

    feat(desktop-work): added swap devices

--- hosts/desktop-work/configuration.nix
+++ hosts/desktop-work/configuration.nix
@@ -5,6 +5,12 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  swapDevices = [
+    {
+      device = "/dev/disk/by-uuid/73de11ef-2cd1-49d2-bc5b-fad43c7d26b7";
+    }
+  ];
+
   networking.firewall.allowedTCPPortsCustom = [];
 
   # Driver Options
