cca640265cb5f2382e84b2c2d88d7f152f02b697
Rodey Seijkens <me@rodey.nl>
2026-09-03 18:43:46 +0200

    refactor: rename hyprland exec-once to exec_once

--- modules/home/audio.nix
+++ modules/home/audio.nix
@@ -65,7 +65,7 @@ in {
     };
 
     # Run audio setup on login
-    wayland.windowManager.hyprland.settings.exec-once = lib.mkAfter [
+    wayland.windowManager.hyprland.settings.exec_once = lib.mkAfter [
       "~/.config/scripts/audio-setup.sh"
     ];
   };
--- modules/home/hyprland/exec-once.nix
+++ modules/home/hyprland/exec-once.nix
@@ -1,30 +1,27 @@
 {...}: let
   terminal = "ghostty";
 in {
-  wayland.windowManager.hyprland = {
-    settings = {
-      # autostart
-      exec-once = [
-        "wl-clip-persist --clipboard regular"
-        "wl-paste --type text --watch cliphist store" # Saves text
-        "wl-paste --type image --watch cliphist store" # Saves images
-        "dbus-update-activation-environment --all --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
-        "systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
-        "systemctl --user start hyprpolkitagent"
+  wayland.windowManager.hyprland.settings = {
+    exec_once = [
+      "wl-clip-persist --clipboard regular"
+      "wl-paste --type text --watch cliphist store"
+      "wl-paste --type image --watch cliphist store"
+      "dbus-update-activation-environment --all --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
+      "systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
+      "systemctl --user start hyprpolkitagent"
 
-        "killall -q waybar;sleep .5 && waybar"
-        "killall -q swaync;sleep .5 && swaync"
-        "killall -q elephant;sleep .5 && elephant"
-        "killall -q walker;sleep .5 && walker --gapplication-service"
-        "killall -q awww;sleep .5 && awww-daemon"
+      "killall -q waybar;sleep .5 && waybar"
+      "killall -q swaync;sleep .5 && swaync"
+      "killall -q elephant;sleep .5 && elephant"
+      "killall -q walker;sleep .5 && walker --gapplication-service"
+      "killall -q awww;sleep .5 && awww-daemon"
 
-        "nm-applet --indicator"
-        "poweralertd"
+      "nm-applet --indicator"
+      "poweralertd"
 
-        "hyprlock"
+      "hyprlock"
 
-        "${terminal} --gtk-single-instance=true --quit-after-last-window-closed=false --initial-window=false"
-      ];
-    };
+      "${terminal} --gtk-single-instance=true --quit-after-last-window-closed=false --initial-window=false"
+    ];
   };
 }
